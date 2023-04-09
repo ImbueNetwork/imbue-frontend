d63789d08e6394bfce8f705a78d05da9bc781ad9
vadik0101 <michael.akio0216@gmail.com>
2023-04-09 01:19:29 +0100

    completed page to submit proposal and login modal

--- src/components/Briefs/BriefInsights.tsx
+++ src/components/Briefs/BriefInsights.tsx
@@ -13,45 +13,58 @@ TimeAgo.addDefaultLocale(en);
 
 export const BriefInsights = ({ brief }: BriefInsightsProps) => {
   const timeAgo = new TimeAgo("en-US");
-  const timePosted = timeAgo.format(new Date(brief.created));
+  const timePosted = timeAgo.format(new Date(brief?.created));
 
   const viewFullBrief = () => {
     redirect(`briefs/${brief.id}/`);
   };
 
   return (
-    <div className="brief-container brief-info">
-      <div className="description">
+    <div className="flex flex-row bg-[#2c2c2c] border border-solid border-[#ffffff40] rounded-[20px] p-[50px]">
+      <div className="flex flex-col gap-[20px] flex-grow flex-shrink-0 basis-[75%] mr-[5%]">
         <div className="brief-title">
-          <h3>{brief.headline}</h3>
-          <span className="clickable-text" onClick={viewFullBrief}>
+          <h3 className="text-xl leading-[1.5] font-bold m-0 p-0">
+            {brief.headline}
+          </h3>
+          <span
+            className="text-[#b2ff0b] cursor-pointer text-base font-bold !m-0 !p-0 relative top-1"
+            onClick={viewFullBrief}
+          >
             View full brief
           </span>
         </div>
         <div className="text-inactive">
-          <p>{brief.description}</p>
+          <p className="text-base">{brief.description}</p>
         </div>
-        <p className="text-inactive">Posted {timePosted} </p>
+        <p className="text-inactive text-base leading-[1.5] font-bold m-0 p-0">
+          Posted {timePosted}{" "}
+        </p>
       </div>
-      <div className="insights">
+      <div className="flex flex-col gap-[50px] flex-grow flex-shrink-0 basis-[20%]">
         <div className="insight-item">
           <RiShieldUserLine color="var(--theme-white)" size={24} />
           <div className="insight-value">
-            <h3>{brief.experience_level}</h3>
+            <h3 className="text-xl leading-[1.5] font-bold m-0 p-0">
+              {brief.experience_level}
+            </h3>
             <div className="text-inactive">Experience Level</div>
           </div>
         </div>
         <div className="insight-item">
           <FaDollarSign color="var(--theme-white)" size={24} />
           <div className="insight-value">
-            <h3>${Number(brief.budget).toLocaleString()}</h3>
+            <h3 className="text-xl leading-[1.5] font-bold m-0 p-0">
+              ${Number(brief.budget).toLocaleString()}
+            </h3>
             <div className="text-inactive">Fixed Price</div>
           </div>
         </div>
         <div className="insight-item">
           <FaRegCalendarAlt color="var(--theme-white)" size={24} />
           <div className="insight-value">
-            <h3>{brief.duration}</h3>
+            <h3 className="text-xl leading-[1.5] font-bold m-0 p-0">
+              {brief.duration}
+            </h3>
             <div className="text-inactive">Project length</div>
           </div>
         </div>
--- src/components/Dialogue.tsx
+++ src/components/Dialogue.tsx
@@ -1,34 +1,50 @@
-import * as React from 'react';
-import {ReactElement} from "react";
+import * as React from "react";
+import { ReactElement } from "react";
 
 export type DialogueProps = {
-    title: String,
-    content?: ReactElement
-    actionList: ReactElement
-}
+  title: String;
+  content?: ReactElement;
+  actionList: ReactElement;
+};
 
 export class Dialogue extends React.Component<DialogueProps> {
-    constructor(props: DialogueProps) {
-        super(props);
-    }
+  constructor(props: DialogueProps) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <div className="mdc-dialog mdc-dialog--open" id="dialog">
-                <div className="mdc-dialog__container">
-                    <div className="mdc-dialog__surface" role="alertdialog" aria-modal="true"
-                         aria-labelledby="dialog-title" aria-describedby="dialog-content"><h2
-                        className="mdc-dialog__title" id="dialog-title">{this.props.title}</h2>
-                        <div className="mdc-dialog__content" id="dialog-content">
-                            {this.props?.content}
-                            <ul className="mdc-deprecated-list mdc-deprecated-list--avatar-list">
-                                {this.props.actionList}
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div className="mdc-dialog__scrim"></div>
+  render() {
+    return (
+      <div
+        className="mdc-dialog mdc-dialog--open flex fixed top-0 left-0 items-center justify-center box-border w-full h-full bg-[#000000bf]"
+        id="dialog"
+      >
+        <div className="mdc-dialog__container flex flex-row items-center justify-around box-border h-full">
+          <div
+            className="mdc-dialog__surface pt-5"
+            role="alertdialog"
+            aria-modal="true"
+            aria-labelledby="dialog-title"
+            aria-describedby="dialog-content"
+          >
+            <h2
+              className="mdc-dialog__title text-xl block relative flex-shrink-0 box-border mx-[24px] mb-[9px] font-medium"
+              id="dialog-title"
+            >
+              {this.props.title}
+            </h2>
+            <div
+              className="mdc-dialog__content py-[20px] px-[24px] text-[#ebeae2] text-base"
+              id="dialog-content"
+            >
+              {this.props?.content}
+              <ul className="mdc-deprecated-list mdc-deprecated-list--avatar-list">
+                {this.props.actionList}
+              </ul>
             </div>
-        );
-    }
+          </div>
+        </div>
+        <div className="mdc-dialog__scrim"></div>
+      </div>
+    );
+  }
 }
