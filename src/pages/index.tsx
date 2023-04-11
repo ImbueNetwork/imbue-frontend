f55df5f208e431272b106ea66fc25276995b0915
samelamin <hussam.elamin@gmail.com>
2023-04-11 14:03:59 +0100

    es lint errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
 import Dashboard from "./dashboard";
 
 export default function Home() {
-  return <Dashboard />;
+  const user = {
+    id: 6,
+    username: "mike",
+    getstream_token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibWlrZSJ9.oSxIRfDYQjN35KF0nx3tINBLh-mlnHKuqIWwxtU_Cnk",
+    display_name: "mike",
+    web3Accounts: [],
+  };
+  
+  return <Dashboard user={user} />;
 }
