b45e406ad4090993e23512d62dc7dcbe14745ffa
vadik0101 <michael.akio0216@gmail.com>
2023-04-11 19:56:27 +0100

    moved login check to sidebar drawer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,10 @@ import { useEffect, useState } from "react";
 import { getCurrentUser } from "@/utils";
 import { User } from "stream-chat";
 import { useRouter } from "next/router";
-import Login from "@/components/Login";
 
 export default function Home() {
   const router = useRouter();
-  const [loginModal, setLoginModal] = useState<boolean>(false);
+
   useEffect(() => {
     getLogedInUser();
   }, []);
@@ -16,18 +15,8 @@ export default function Home() {
     const userResponse = await getCurrentUser();
     if (userResponse) {
       router.push("/dashboard");
-    } else {
-      setLoginModal(true);
     }
   };
 
-  return (
-    <>
-      <Login
-        visible={loginModal}
-        setVisible={setLoginModal}
-        redirectUrl="/dashboard"
-      />
-    </>
-  );
+  return <></>;
 }
