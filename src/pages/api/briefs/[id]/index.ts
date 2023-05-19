61877a9aa11b69079dfa9fe05f3cf130a8ccef1b
vadik0101 <michael.akio0216@gmail.com>
2023-05-19 12:36:48 +0100

    refacor for brief search and brief detail

--- src/pages/api/briefs/[id]/index.ts
+++ src/pages/api/briefs/[id]/index.ts
@@ -2,68 +2,52 @@ import { NextApiRequest, NextApiResponse } from "next";
 import db from "../../db";
 import * as models from "../../models";
 import { Brief, BriefSqlFilter, fetchItems } from "../../models";
+import nextConnect from "next-connect";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const { id: briefId } = req.query;
+export default nextConnect()
+  .get(async (req: NextApiRequest, res: NextApiResponse) => {
+    const { id } = req.query;
+    if (!id) return;
+    let response;
+    await db.transaction(async (tx) => {
+      try {
+        const brief = await models.fetchBrief(id)(tx);
+        await Promise.all([
+          (brief.skills = await fetchItems(brief.skill_ids, "skills")(tx)),
+          (brief.industries = await fetchItems(
+            brief.industry_ids,
+            "industries"
+          )(tx)),
+        ]);
+        response = brief;
+      } catch (e) {
+        response = new Error(`Failed to fetch brief with id ${id}`, {
+          cause: e as Error,
+        });
+      }
+    });
+    res.status(200).json(response);
+  })
+  .post(async (req: NextApiRequest, res: NextApiResponse) => {
+    const data = req.body as BriefSqlFilter;
+    await db.transaction(async (tx: any) => {
+      try {
+        const briefs: Array<Brief> = await models.searchBriefs(tx, data);
 
-  if (briefId === "search") {
-    const result = await searchBriefs(req);
+        await Promise.all([
+          briefs,
+          ...briefs.map(async (brief: any) => {
+            brief.skills = await fetchItems(brief.skill_ids, "skills")(tx);
+            brief.industries = await fetchItems(
+              brief.industry_ids,
+              "industries"
+            )(tx);
+          }),
+        ]);
 
-    return res.status(200).json(result);
-  }
-  else {
-    if (!briefId) return;
-
-    const response = await getBrief(briefId)
-    return res.status(200).json(response)
-  }
-}
-
-export async function getBrief(id: string | string[]) {
-  if (!id) return
-
-  let response;
-  await db.transaction(async tx => {
-    try {
-      const brief = await models.fetchBrief(id)(tx);
-      await Promise.all([
-        brief.skills = await fetchItems(brief.skill_ids, "skills")(tx),
-        brief.industries = await fetchItems(brief.industry_ids, "industries")(tx),
-      ]);
-      response = brief
-    } catch (e) {
-      response = (new Error(
-        `Failed to fetch brief with id ${id}`,
-        { cause: e as Error }
-      ));
-    }
+        res.status(200).json(briefs);
+      } catch (e) {
+        new Error(`Failed to search for briefs ${data}`, { cause: e as Error });
+      }
+    });
   });
-  return response
-}
-
-export async function searchBriefs(req: NextApiRequest) {
-  let response;
-  const data = req.body as BriefSqlFilter;
-  await db.transaction(async (tx: any) => {
-    try {
-      const briefs: Array<Brief> = await models.searchBriefs(tx, data);
-
-      await Promise.all([
-        briefs,
-        ...briefs.map(async (brief: any) => {
-          brief.skills = await fetchItems(brief.skill_ids, "skills")(tx);
-          brief.industries = await fetchItems(brief.industry_ids, "industries")(tx);
-        }),
-      ]);
-
-      response = briefs;
-    } catch (e) {
-      new Error(`Failed to search for briefs ${data}`, { cause: e as Error });
-    }
-  });
-
-  return response;
-}
