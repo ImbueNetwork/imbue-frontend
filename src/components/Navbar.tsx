baddd273c5fa00676aeb67703d9bee1431e5ec30
ssani7 <sanaullah.sani756@gmail.com>
2023-06-01 00:33:30 +0600

    created full sized working navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,53 @@
 import { appLogo } from "@/assets/svgs";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Drawer from "@/components/Drawer";
+import { Avatar, Box, Divider, IconButton, ListItemIcon, Menu, MenuItem, Tooltip, Typography } from "@mui/material";
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+import { getCurrentUser } from "@/utils";
+import { User } from "@/model";
+import { freelancerExists } from "@/redux/services/freelancerService";
+import { getFreelancerProfile } from "@/redux/services/freelancerService";
+import defaultProfile from "../assets/images/profile-image.png"
+import MenuItems from "./Navbar/MenuItems";
+import Login from "./Login";
 
 const logoStyle = { height: "100%", width: "100%" };
 
 function Navbar() {
-  const [sideBarIsVisivle, setSideBarIsVisible] = useState<boolean>(false);
+  const [loginModal, setLoginModal] = useState<boolean>(false);
+  const [redirectURL, setRedirectURL] = useState<string>();
+  const [freelancerProfile, setFreelancerProfile] = useState<any>()
+  const [user, setUser] = useState<User>()
 
-  const toggleSideBar = (): void => {
-    setSideBarIsVisible(!sideBarIsVisivle);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
   };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  useEffect(() => {
+    const setup = async () => {
+      const user = await getCurrentUser() || false;
+      if (user) {
+        const res = await getFreelancerProfile(user.username)
+        setFreelancerProfile(res);
+        setUser(user);
+      }
+    };
+    setup();
+  }, []);
+
+
   return (
     <>
-      <header className="py-[15px] lg:py-[30px]" id="header-wrapper">
-        <div id="main-header">
+      <header className="py-3 navBar" id="header-wrapper">
+        <div id="main-header" className="flex justify-between">
           <h1 className="main-title">
             <Link href="/">
               <div id="logo">
@@ -23,21 +55,85 @@ function Navbar() {
               </div>
             </Link>
           </h1>
-          <div className="context-menu" id="context-menu">
+          {/* <div className="context-menu" id="context-menu">
             <div className="context-menu-item" onClick={toggleSideBar}>
               <a id="main-menu" className="material-icons">
                 &#xe5d2;
               </a>
             </div>
-          </div>
+          </div> */}
+
+          <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+            <Typography sx={{ minWidth: 100 }}>Contact</Typography>
+            <Typography sx={{ minWidth: 100 }}>Profile</Typography>
+            <Tooltip title="Account settings">
+              <IconButton
+                onClick={handleClick}
+                size="small"
+                sx={{ ml: 2 }}
+                aria-controls={open ? 'account-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+              >
+                <Avatar sx={{ width: 40, height: 40 }}>
+                  <Image src={freelancerProfile?.profile_image || defaultProfile} width={40} height={40} alt="profile" />
+                </Avatar>
+              </IconButton>
+            </Tooltip>
+          </Box>
         </div>
+        <Menu
+          anchorEl={anchorEl}
+          id="account-menu"
+          open={open}
+          onClose={handleClose}
+          onClick={handleClose}
+          className="navBar"
+          PaperProps={{
+            elevation: 0,
+            sx: {
+              overflow: 'visible',
+              bgcolor: "#0F0F0F",
+              color: "white",
+              filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+              mt: 1.5,
+              '& .MuiAvatar-root': {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              '&:before': {
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: '#0F0F0F',
+                transform: 'translateY(-50%) rotate(45deg)',
+                zIndex: 0,
+              },
+            },
+          }}
+          transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        >
+          <MenuItems
+            isFreelancer={freelancerProfile?.id ? true : false}
+            {...{ user, setLoginModal }}
+          />
+        </Menu>
       </header>
-      <div
-        onClick={toggleSideBar}
-        className={`modal ${sideBarIsVisivle ? "show" : ""}`}
-        id="modal"
+      <Login
+        visible={loginModal}
+        setVisible={(val) => {
+          console.log("object");
+          setLoginModal(val);
+        }}
+        redirectUrl={redirectURL || "/dashboard"}
       />
-      <Drawer visible={sideBarIsVisivle} setSideBarIsVisible={setSideBarIsVisible} />
     </>
   );
 }
---
+++ src/components/Navbar/MenuItems.tsx
@@ -0,0 +1,91 @@
+import { ListItemIcon, MenuItem } from '@mui/material';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Login from '../Login';
+
+
+const MenuItems = ({ user, isFreelancer, setLoginModal }: any) => {
+    const router = useRouter();
+    const [redirectURL, setRedirectURL] = useState<string>();
+
+    const linkItems = [
+        {
+            icon: "face",
+            text: "Dashboard",
+            link: "/dashboard",
+            needAuthentication: true,
+        },
+        {
+            icon: "work",
+            text: "Submit A Brief",
+            link: "/briefs/new",
+            needAuthentication: true,
+        },
+        {
+            icon: "search",
+            text: "Discover Briefs",
+            link: "/briefs",
+            needAuthentication: false,
+        },
+        {
+            icon: "groups",
+            text: "Discover Freelancers",
+            link: `/freelancers`,
+            needAuthentication: false,
+        },
+        {
+            icon: isFreelancer ? "account_circle" : "group_add",
+            text: isFreelancer ? "Freelancer Profile" : "Join The Freelancers",
+            link: isFreelancer ? `/freelancers/${user?.username}/` : "/freelancers/new",
+            needAuthentication: true,
+        },
+        {
+            icon: "money",
+            text: "Transfer Funds",
+            link: "/relay",
+            needAuthentication: false,
+        },
+        {
+            icon: "logout",
+            text: user?.username ? "Sign Out" : "Sign In",
+            link: user?.username ? "/logout" : "/login",
+            needAuthentication: true,
+        },
+    ];
+
+    const navigateToPage = async (link: string, needAuthentication: boolean) => {
+        if (needAuthentication && !user?.username) {
+            setRedirectURL(router.asPath)
+            setLoginModal(true);
+        }
+        else {
+            router.push(link);
+        }
+    };
+    return (
+        <>
+            <div className='menuItems flex flex-col gap-2'>
+                {
+                    linkItems.map((item, index) => (
+                        <MenuItem key={index} onClick={() => navigateToPage(item.link, item.needAuthentication)}>
+                            <ListItemIcon>
+                                <i
+                                    className="material-icons relative top-[4px] text-white"
+                                    aria-hidden="true"
+                                >
+                                    {item?.icon}
+                                </i>
+                            </ListItemIcon>
+                            <p>
+                                {item?.text}
+                            </p>
+                        </MenuItem>
+                    ))
+                }
+            </div>
+        </>
+    );
+};
+
+export default MenuItems;
