b602cc2fe925f6e74d05a025f1bd94220f19e6f2
ssani7 <sanaullah.sani756@gmail.com>
2023-09-23 17:58:38 +0600

    very small screen fix

--- src/pages/auth/sign-up/index.tsx
+++ src/pages/auth/sign-up/index.tsx
@@ -269,7 +269,7 @@ export default function SignIn() {
 
   return (
     // <div className="w-full max-width-400px:mt-0 -mt-[100px] lg:mt-0 h-screen lg:h-auto flex justify-center items-center">
-    <div className="flex justify-center items-center absolute inset-0">
+    <div className="flex justify-center items-center absolute top-0 min-width-376px:inset-0">
       <div className="bg-white flex sm:space-x-5 p-2 rounded-2xl mx-4">
         <div className="left-side">
           <div className="left-side hidden lg:block w-[28rem] h-full lg:w-[31.25rem]">
