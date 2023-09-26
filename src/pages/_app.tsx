13bf91ea016a6bb048d7ade528c057292a9043d7
samelamin <hussam.elamin@gmail.com>
2023-09-26 16:49:14 +0100

    reduce bundle size

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,9 @@
 import { createTheme, ThemeProvider } from '@mui/material';
 import { StyledEngineProvider } from '@mui/material/styles';
 import type { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
 import Head from 'next/head';
-import '@/styles/common.css';
-import '@/styles/globals.css';
-import '@/styles/index.css';
-import '@/styles/proposal.css';
-import '@/styles/briefs.css';
-import '@/styles/new-brief.css';
-import '@/styles/brief-details.css';
-import '@/styles/submit-proposal.css';
-import '@/styles/muiGlobal.css';
-import '@/styles/stream-chat.css';
-import '@/styles/animation.css';
-
-import Layout from '@/components/Layout';
+const Layout = dynamic(() => import("@/components/Layout"));
 
 export default function App({ Component, pageProps }: AppProps) {
   const theme = createTheme({
