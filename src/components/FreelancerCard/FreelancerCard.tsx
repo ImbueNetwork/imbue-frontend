1f39283d791b51549d887fa3d1a0fdc8807fd9c9
Riyad139 <riyad.ahmed1886@gmail.com>
2023-11-08 20:18:03 +0600

    1. fix percentage error

--- src/components/FreelancerCard/FreelancerCard.tsx
+++ src/components/FreelancerCard/FreelancerCard.tsx
@@ -66,7 +66,9 @@ export default function FreelancerCard({
         </p>
         <p className='text-xs'>
           Job Success rate{' '}
-          <span className='text-imbue-purple'>{freelancerSuccsRate}%</span>
+          <span className='text-imbue-purple'>
+            {Number(freelancerSuccsRate).toFixed(2)}%
+          </span>
         </p>
       </div>
       <div className='flex mt-10 text-xs gap-5'>
